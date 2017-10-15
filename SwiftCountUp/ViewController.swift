af372a2891145eafffc198c593b76014cef9066f
kter <me@kter.jp>
2017-10-15 21:38:09 +0900

    fix

--- SwiftCountUp/ViewController.swift
+++ SwiftCountUp/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scoreBoard: UILabel!
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        scoreBoard.text = String(score)
     }
 
+    @IBAction func countUp(_ sender: Any) {
+        score += 1
+        scoreBoard.text = String(score)
+    }
+    
+    @IBAction func countDown(_ sender: Any) {
+        score -= 1
+        scoreBoard.text = String(score)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
